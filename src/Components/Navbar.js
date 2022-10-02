c6fb05878212394a314cc1a8bd39128adb1d4aa2
Parth <parthsood99@gmail.com>
2022-10-02 15:07:03 +0530

    Edited navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,8 +2,25 @@ import React from "react"
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>This is navbar</h1>
+    <div className="uppersec">
+      <nav className="navbar1">
+      {/* <h1>This is navbar</h1> */}
+      <div className="upperLink">
+        <a href="/">SPONSOR</a>
+        <a href="/">REGISTER</a>
+
+      </div>
+         <div className="link">
+         <a href="/">CONFERENCE</a>
+         <a href="/">EXHIBITION</a>
+         <a href="/">ATTEND</a>
+         <a href="/">PRESS</a>
+         <a href="/">()</a>
+
+
+          </div>
+      </nav>
+      
     </div>
   )
 }
