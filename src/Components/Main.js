5c87e6844db5eb3c68a7e555c75db14a4ea5a6d7
Preet-Sojitra <preet.dev373@gmail.com>
2022-10-02 20:24:46 +0530

    add main section

---
+++ src/Components/Main.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+  return (
+    <div className="main">
+      <div className="main-text-container">
+        <h1 className="main-heading">Thrive Together</h1>
+        <p className="main-desc">
+          Recharge and reboot at SIGGRAPH 2019, as multifaceted as you
+          are—featuring global innovators who are changing what’s possible in
+          computer graphics, animation, VR, Gaming, and Emerging Technologies.
+        </p>
+      </div>
+    </div>
+  )
+}
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,23 +4,19 @@ export default function Navbar() {
   return (
     <div className="uppersec">
       <nav className="navbar1">
-      {/* <h1>This is navbar</h1> */}
-      <div className="upperLink">
-        <a href="/">SPONSOR</a>
-        <a href="/">REGISTER</a>
-
-      </div>
-         <div className="link">
-         <a href="/">CONFERENCE</a>
-         <a href="/">EXHIBITION</a>
-         <a href="/">ATTEND</a>
-         <a href="/">PRESS</a>
-         <a href="/">()</a>
-
-
-          </div>
+        {/* <h1>This is navbar</h1> */}
+        <div className="upperLink">
+          <a href="/">SPONSOR</a>
+          <a href="/">REGISTER</a>
+        </div>
+        <div className="link">
+          <a href="/">CONFERENCE</a>
+          <a href="/">EXHIBITION</a>
+          <a href="/">ATTEND</a>
+          <a href="/">PRESS</a>
+          <a href="/">()</a>
+        </div>
       </nav>
-      
     </div>
   )
 }
