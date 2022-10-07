87a05cfa3944b1e6cd6ef5a63f72ab60fc5e9b2c
Preet-Sojitra <preet.dev373@gmail.com>
2022-10-08 01:31:37 +0530

    add carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar"
 import Main from "./Components/Main"
+import Carousel from "./Components/Carousel"
 
 function App() {
   return (
     <div className="landing-page">
       <Navbar />
       <Main />
+      <Carousel />
     </div>
   )
 }
--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,2 +1,69 @@
 import React from "react"
+import IndividualCarousel from "./IndividualCarousel"
 import carouseldata from "../assets/data/carouseldata"
+import "../assets/css/carousel.css"
+
+export default function Carousel() {
+  const [count, setCount] = React.useState(0)
+  const [translateX, setTranslateX] = React.useState(0)
+
+  const carousels = carouseldata.map((carousel) => {
+    return (
+      <IndividualCarousel
+        key={carousel.key}
+        id={carousel.key}
+        {...carousel}
+        translateX={translateX}
+      />
+    )
+  })
+
+  const translateCarousel = (e) => {
+    e.target.id === "prev"
+      ? setCount((prevCount) => {
+          if (prevCount - 1 === -1) {
+            return carousels.length - 1
+          }
+          return (prevCount = prevCount - 1)
+        })
+      : setCount((prevCount) => {
+          if (prevCount === carousels.length - 1) {
+            return 0
+          }
+          return (prevCount = prevCount + 1)
+        })
+
+    e.target.id === "prev"
+      ? setTranslateX((prevTranslateX) => {
+          if (prevTranslateX === 0) {
+            return -((carouseldata.length - 1) * 100)
+          }
+          return prevTranslateX + 100
+        })
+      : setTranslateX((prevTranslateX) => {
+          if (prevTranslateX === -((carouseldata.length - 1) * 100)) {
+            return 0
+          }
+          return prevTranslateX - 100
+        })
+  }
+
+  return (
+    <div className="carousels-section">
+      <div className="allCarousels-container">{carousels[count]}</div>
+      <div className="btns-container">
+        <button className="btn prev-btn" id="prev" onClick={translateCarousel}>
+          Previous
+        </button>
+        <button className="btn next-btn" id="next" onClick={translateCarousel}>
+          Next
+        </button>
+      </div>
+      <div className="indicators">
+        <div className="indicator active-indicator" id="0"></div>
+        <div className="indicator" id="1"></div>
+        <div className="indicator" id="2"></div>
+      </div>
+    </div>
+  )
+}
