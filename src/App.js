9d1cac5c572ce9b42cbf6aa2e08306bcae108658
Parth <parthsood99@gmail.com>
2022-10-08 22:32:31 +0530

    Footer and sponsors done minor changes left

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Navbar from "./Components/Navbar"
 import Main from "./Components/Main"
 import Section2 from "./Components/Section2"
-
+import Sponsor from "./Components/sponsor"
+import Footer from "./Components/footer"
 function App() {
   return ( 
     <div className="landing-page">
       <Navbar />
       <Main />
       <Section2 />
+      <Sponsor />
+      <Footer />
     </div>
   )
 }
---
+++ src/Components/sponsor.js
@@ -0,0 +1,27 @@
+import React from "react"
+import "../assets/css/sponsor.css"
+
+
+export default function Sponsor(){
+  return ( 
+
+    <div>
+        <div className="topic">Thanks to our Sponsors</div>
+
+        <div className="tab-container">
+        <div className="tabs"></div>
+        <div className="tabs"></div>
+        <div className="tabs"></div>
+        <div className="tabs"></div>
+        <div className="tabs"></div>
+        <div className="tabs"></div>
+        <div className="tabs"></div>
+        <div className="tabs"></div>
+        <div className="tabs"></div>
+        <div className="tabs"></div>
+        <div className="tabs"></div>
+        <div className="tabs"></div>
+        </div>
+    </div>
+  )
+}
