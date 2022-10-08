8eadb81d17a3c10532c728f909d19a137cb2f183
Parth <parthsood99@gmail.com>
2022-10-08 18:22:48 +0530

    added section 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar"
 import Main from "./Components/Main"
+import Section2 from "./Components/Section2"
 
 function App() {
-  return (
+  return ( 
     <div className="landing-page">
       <Navbar />
       <Main />
+      <Section2 />
     </div>
   )
 }
