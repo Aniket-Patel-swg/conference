3ec50cabb3eaee1d411ac25ec6887d3782923048
Aniket Patel <111775702+Aniket-Patel-swg@users.noreply.github.com>
2022-11-02 13:13:20 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import Section2 from "./Components/Section2"
 import Sponsor from "./Components/sponsor"
 import Footer from "./Components/footer"
 import Carousel from "./Components/Carousel"
+import CommingSoon from "./Components/CommingSoon"
+import HomePage from "./Pages/HomePage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="landing-page">
+  <BrowserRouter>
+  <Routes>
+
+    {/* <div className="landing-page">
       <Navbar />
       <Main />
       <Section2 />
       <Carousel />
       <Sponsor />
       <Footer />
-    </div>
+    </div> */}
+    <Route path="/" element = { <HomePage /> }/>
+    <Route path="/CommingSoon" element = { <CommingSoon />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
